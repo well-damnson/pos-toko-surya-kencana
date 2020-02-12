c9e822af3a0072ee14b67b4b701bbef84ae4df48
AttreaVe <johansiautama@gmail.com>
2020-02-12 13:39:27 +0700

    Initial Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { View } from 'react-native';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <View style={{ width: 100, height: 20, backgroundColor: 'red' }} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
