ac3ab3cb98509e1dd62490bd116e04c784a5b8aa
davsobo <aureliousvayne57@gmail.com>
2020-04-15 16:45:41 +0700

    try modal Beli

--- frontend/src/modals/modalTambahItem.js
+++ frontend/src/modals/modalTambahItem.js
@@ -10,38 +10,105 @@ import {
   Form,
   Picker,
 } from "native-base";
-import { View } from "react-native";
+import { View, TextInput } from "react-native";
 import { Col, Row, Grid } from "react-native-easy-grid";
 
 import Hook from "@/wrapper";
 
 let ModalTambahItem = (props) => {
-  let { Client } = Hook.useClientState();
+  let defaultState = {
+    nama: "",
+    jenis: "-",
+    jenisBaru: "",
+    berat: "",
+    kadar: "",
+    posisi: "-",
+    posisiBaru: "",
+    picture: "",
+    beli: "",
+  };
 
   let [state, setState] = useState({
-    nama: "",
-    jenis: "",
-    berat: 0,
-    kadar: 0,
-    posisi: "",
-    picture: "",
-    beli: 0,
+    ...defaultState,
   });
-  let [posisi, setPosisi] = useState([]);
-  let [jenis, setJenis] = useState([]);
+
+  let [toSubmit, setToSubmit] = useState({});
 
   useEffect(() => {
-    let fetchFunction = async () => {
-      let itemsService = Client.service("item-area");
-      let data = await itemsService.find();
-      console.log(data);
-    };
-    fetchFunction();
-  }, []);
+    let {
+      nama,
+      jenis,
+      jenisBaru,
+      berat,
+      kadar,
+      posisi,
+      posisiBaru,
+      beli,
+      picture,
+    } = state;
+    setToSubmit({
+      nama,
+      jenis: jenis === "-" ? jenisBaru : jenis,
+      berat,
+      kadar,
+      posisi: posisi === "-" ? posisiBaru : posisi,
+      beli,
+      picture,
+    });
+  }, [state]);
 
-  let setValue = (key, value) => {
+  let [jenisList, setJenisList] = useState([]);
+  let [posisiList, setPosisiList] = useState([]);
+
+  let { Client } = Hook.useClientState();
+
+  let setter = (key, value, numberOnly = false) => {
+    if (numberOnly) {
+      value = value.replace(/[^\d.-]/g, "");
+    }
     setState((state) => ({ ...state, [key]: value }));
   };
+  useEffect(() => {
+    let jenisList = {};
+    let posisList = {};
+    let dataFetch = async () => {
+      let itemAreaServices = Client.service("item-area");
+      let data = await itemAreaServices.find();
+      data.forEach(({ posisi, jenis }) => {
+        jenisList[jenis] = true;
+        posisiList[posisi] = true;
+      });
+      setJenisList(Object.keys(jenisList));
+      setPosisiList(Object.keys(posisiList));
+    };
+    dataFetch();
+  }, []);
+
+  let submit = async () => {
+    console.log("submit pressed");
+    let state = toSubmit;
+    console.log(state);
+    if (
+      state.nama.length &&
+      (state.jenis !== "-" || state.jenisBaru.length) &&
+      state.berat.length &&
+      state.kadar.length &&
+      (state.posisi !== "-" || state.posisiBaru.length) &&
+      state.beli.length
+    ) {
+      try {
+        let services = Client.service("item-area");
+        let result = await services.create(state);
+        console.log(result);
+        setState({ ...defaultState });
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  };
+
+  console.log(state);
+
   return (
     <Container>
       <Content>
@@ -61,7 +128,7 @@ let ModalTambahItem = (props) => {
             >
               <Input
                 value={state.nama}
-                onChangeText={(text) => setValue("nama", text)}
+                onChangeText={(text) => setter("nama", text)}
                 style={{ height: "3vh" }}
               />
             </Item>
@@ -71,18 +138,6 @@ let ModalTambahItem = (props) => {
           <Row style={{ margin: 10 }}>
             <Text style={{ flex: 1.5 }}>Jenis:</Text>
             <View style={{ flex: 0.2 }} />
-            {/* <Item
-                style={{
-                  flex: 2,
-                  alignSelf: "center",
-                  height: "3vh",
-                  backgroundColor: "#FFF",
-                  width: "20vw"
-                }}
-                regular
-              >
-                <Input style={{ height: "3vh" }} />
-              </Item> */}
             <Form
               style={{
                 flex: 2,
@@ -90,23 +145,43 @@ let ModalTambahItem = (props) => {
                 height: "3vh",
                 backgroundColor: "#FFF",
                 width: "20vw",
+                paddingLeft: state.jenis === "-" ? 30 : 0,
               }}
             >
               <Picker
                 mode="dropdown"
                 placeholder="jenis"
-                placeholderStyle={{ color: "#bfc6ea" }}
-                placeholderIconColor="#007aff"
+                // placeholderStyle={{ color: "#bfc6ea" }}
+                // placeholderIconColor="#007aff"
                 style={{ width: undefined }}
                 selectedValue={state.jenis}
-                onValueChange={(text) => setValue("jenis", text)}
+                onValueChange={(value) => {
+                  setter("jenis", value);
+                }}
               >
-                <Picker.Item label="Kalung" value="key1" />
-                <Picker.Item label="Liontin" value="key2" />
-                <Picker.Item label="Anting" value="key3" />
-                <Picker.Item label="Cincin" value="key4" />
+                <Picker.Item label="Buat Baru" value="-" />
+                {jenisList.map((value, index) => (
+                  <Picker.Item key={index} label={value} value={value} />
+                ))}
               </Picker>
             </Form>
+            <div
+              style={{
+                alignSelf: "center",
+                display: state.jenis === "-" ? "block" : "none",
+                marginLeft: 20,
+              }}
+            >
+              <TextInput
+                style={{
+                  width: "10vw",
+                  height: "3vh",
+                  borderWidth: "1px",
+                }}
+                value={state.jenisBaru}
+                onChangeText={(text) => setter("jenisBaru", text)}
+              />
+            </div>
             <View style={{ flex: 2, flexGrow: 10, flexBasis: 25 }} />
           </Row>
 
@@ -125,7 +200,7 @@ let ModalTambahItem = (props) => {
             >
               <Input
                 value={state.berat}
-                onChangeText={(text) => setValue("berat", text)}
+                onChangeText={(text) => setter("berat", text)}
                 style={{ height: "3vh" }}
               />
             </Item>
@@ -148,7 +223,7 @@ let ModalTambahItem = (props) => {
             >
               <Input
                 value={state.kadar}
-                onChangeText={(text) => setValue("kadar", text)}
+                onChangeText={(text) => setter("kadar", text)}
                 style={{ height: "3vh" }}
               />
             </Item>
@@ -166,6 +241,7 @@ let ModalTambahItem = (props) => {
                 height: "3vh",
                 backgroundColor: "#FFF",
                 width: "20vw",
+                paddingLeft: state.posisi === "-" ? 30 : 0,
               }}
             >
               <Picker
@@ -175,15 +251,31 @@ let ModalTambahItem = (props) => {
                 placeholderIconColor="#007aff"
                 style={{ width: undefined }}
                 selectedValue={state.posisi}
-                onValueChange={(text) => setValue("posisi", text)}
+                onValueChange={(value) => setter("posisi", value)}
               >
-                <Picker.Item label="Pilih" value="key0" />
-                <Picker.Item label="A1" value="key1" />
-                <Picker.Item label="A2" value="key2" />
-                <Picker.Item label="B1" value="key3" />
-                <Picker.Item label="B2" value="key4" />
+                <Picker.Item label="Buat Baru" value="-" />
+                {posisiList.map((value, index) => (
+                  <Picker.Item key={index} label={value} value={value} />
+                ))}
               </Picker>
             </Form>
+            <div
+              style={{
+                alignSelf: "center",
+                display: state.posisi === "-" ? "block" : "none",
+                marginLeft: 20,
+              }}
+            >
+              <TextInput
+                style={{
+                  width: "10vw",
+                  height: "3vh",
+                  borderWidth: "1px",
+                }}
+                value={state.posisiBaru}
+                onChangeText={(text) => setter("posisiBaru", text)}
+              />
+            </div>
             <View style={{ flex: 2, flexGrow: 10, flexBasis: 25 }} />
           </Row>
 
@@ -224,7 +316,7 @@ let ModalTambahItem = (props) => {
               <Input
                 style={{ height: "3vh" }}
                 value={state.beli}
-                onChangeText={(text) => setValue('beli', text)}
+                onChangeText={(text) => setter("beli", text)}
               />
             </Item>
             <View style={{ flex: 2, flexGrow: 10, flexBasis: 25 }} />
@@ -291,18 +383,6 @@ class ContentExample extends Component {
             <Row style={{ margin: 10 }}>
               <Text style={{ flex: 1.5 }}>Jenis:</Text>
               <View style={{ flex: 0.2 }} />
-              {/* <Item
-                style={{
-                  flex: 2,
-                  alignSelf: "center",
-                  height: "3vh",
-                  backgroundColor: "#FFF",
-                  width: "20vw"
-                }}
-                regular
-              >
-                <Input style={{ height: "3vh" }} />
-              </Item> */}
               <Form
                 style={{
                   flex: 2,
