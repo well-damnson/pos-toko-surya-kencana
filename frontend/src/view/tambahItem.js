b39db169c08b92e0c30c08e735ec5a222725538a
AttreaVe <johansiautama@gmail.com>
2020-04-15 13:29:09 +0700

    tambah item

--- frontend/src/view/tambahItem.js
+++ frontend/src/view/tambahItem.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from 'react';
 import {
   Container,
   Item,
@@ -8,56 +8,88 @@ import {
   StyleProvider,
   Button,
   Form,
-  Picker
-} from "native-base";
-import { View } from "react-native";
-import { Col, Row, Grid } from "react-native-easy-grid";
+  Picker,
+} from 'native-base';
+import { View } from 'react-native';
+import { Col, Row, Grid } from 'react-native-easy-grid';
 
-export default class ContentExample extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selected: undefined,
-      selected2: undefined
+import Hook from '@/wrapper';
+
+let TambahItem = () => {
+  let defaultState = {
+    nama: '',
+    jenis: '',
+    jenisBaru: '',
+    berat: '',
+    kadar: '',
+    posisi: '',
+    posisiBaru: '',
+    picture: '',
+    beli: '',
+  };
+
+  let [state, setState] = useState({
+    ...defaultState,
+  });
+
+  let [jenisList, setJenisList] = useState([]);
+  let [posisiList, setPosisiList] = useState([]);
+
+  let { Client } = Hook.useClientState();
+
+  let setter = (key, value, numberOnly = false) => {
+    if (numberOnly) {
+      value = value.replace(/[^\d.-]/g, '');
+    }
+    setState((state) => ({ ...state, [key]: value }));
+  };
+  useEffect(() => {
+    let jenisList = {};
+    let posisList = {};
+    let dataFetch = async () => {
+      let itemAreaServices = Client.service('item-area');
+      let data = await itemAreaServices.find();
+      data.forEach(({ posisi, jenis }) => {
+        jenisList[jenis] = true;
+        posisiList[posisi] = true;
+      });
+      setJenisList(Object.keys(jenisList));
+      setPosisiList(Object.keys(posisiList));
     };
-  }
-  onValueChange(value: string) {
-    this.setState({
-      selected: value
-    });
-  }
-  onValueChange2(value: string) {
-    this.setState({
-      selected2: value
-    });
-  }
-  render() {
-    return (
-      <Container>
-        <Content>
-          <Grid>
-            <Row style={{ margin: 10 }}>
-              <Text style={{ flex: 1.5 }}>Nama:</Text>
-              <View style={{ flex: 0.2 }} />
-              <Item
-                style={{
-                  flex: 2,
-                  alignSelf: "center",
-                  height: "3vh",
-                  backgroundColor: "#FFF",
-                  width: "20vw"
-                }}
-                regular
-              >
-                <Input style={{ height: "3vh" }} />
-              </Item>
-              <View style={{ flex: 2, flexGrow: 10, flexBasis: 25 }} />
-            </Row>
+    dataFetch();
+  }, []);
 
-            <Row style={{ margin: 10 }}>
-              <Text style={{ flex: 1.5 }}>Jenis:</Text>
-              <View style={{ flex: 0.2 }} />
-              {/* <Item
+  console.log(state);
+  return (
+    <Container>
+      <Content>
+        <Grid>
+          <Row style={{ margin: 10 }}>
+            <Text style={{ flex: 1.5 }}>Nama:</Text>
+            <View style={{ flex: 0.2 }} />
+            <Item
+              style={{
+                flex: 2,
+                alignSelf: 'center',
+                height: '3vh',
+                backgroundColor: '#FFF',
+                width: '20vw',
+              }}
+              regular
+            >
+              <Input
+                style={{ height: '3vh' }}
+                value={state.nama}
+                onChangeText={(text) => setter('nama', text)}
+              />
+            </Item>
+            <View style={{ flex: 2, flexGrow: 10, flexBasis: 25 }} />
+          </Row>
+
+          <Row style={{ margin: 10 }}>
+            <Text style={{ flex: 1.5 }}>Jenis:</Text>
+            <View style={{ flex: 0.2 }} />
+            {/* <Item
                 style={{
                   flex: 2,
                   alignSelf: "center",
@@ -69,152 +101,189 @@ export default class ContentExample extends Component {
               >
                 <Input style={{ height: "3vh" }} />
               </Item> */}
-              <Form
-                style={{
-                  flex: 2,
-                  alignSelf: "center",
-                  height: "3vh",
-                  backgroundColor: "#FFF",
-                  width: "20vw"
+            <Form
+              style={{
+                flex: 2,
+                alignSelf: 'center',
+                height: '3vh',
+                backgroundColor: '#FFF',
+                width: '20vw',
+              }}
+            >
+              <Picker
+                mode="dropdown"
+                // placeholder="Select your SIM"
+                // placeholderStyle={{ color: '#bfc6ea' }}
+                // placeholderIconColor="#007aff"
+                style={{ width: undefined }}
+                selectedValue={state.jenis}
+                onValueChange={(value) => {
+                  setter('jenis', value);
                 }}
               >
-                <Picker
-                  mode="dropdown"
-                  placeholder="Select your SIM"
-                  placeholderStyle={{ color: "#bfc6ea" }}
-                  placeholderIconColor="#007aff"
-                  style={{ width: undefined }}
-                  selectedValue={this.state.selected}
-                  onValueChange={this.onValueChange.bind(this)}
-                >
-                  <Picker.Item label="Pilih" value="key0" />
-                  <Picker.Item label="Kalung" value="key1" />
-                  <Picker.Item label="Liontin" value="key2" />
-                  <Picker.Item label="Anting" value="key3" />
-                  <Picker.Item label="Cincin" value="key4" />
-                </Picker>
-              </Form>
-              <View style={{ flex: 2, flexGrow: 10, flexBasis: 25 }} />
-            </Row>
-
-            <Row style={{ margin: 10 }}>
-              <Text style={{ flex: 1.5 }}>Berat: </Text>
-              <View style={{ flex: 0.2 }} />
-              <Item
+                <Picker.Item label="Buat Baru" value="-" />
+                {jenisList.map((value, index) => (
+                  <Picker.Item key={index} label={value} value={value} />
+                ))}
+              </Picker>
+            </Form>
+            <div
+              style={{
+                display: state.jenis === '-' ? 'block' : 'none',
+                marginLeft: '5px',
+              }}
+            >
+              <Input
                 style={{
-                  flex: 1.5,
-                  alignSelf: "center",
-                  height: "3vh",
-                  backgroundColor: "#FFF",
-                  width: "20vw"
+                  borderWidth: '1px',
                 }}
-                regular
-              >
-                <Input style={{ height: "3vh" }} />
-              </Item>
-              <Text style={{ marginHorizontal: 10 }}>gr</Text>
-              <View style={{ flex: 2, flexGrow: 10, flexBasis: 25 }} />
-            </Row>
+                value={state.jenisBaru}
+                onChangeText={(text) => setter('jenisBaru', text)}
+              />
+            </div>
+            <View style={{ flex: 2, flexGrow: 10, flexBasis: 25 }} />
+          </Row>
 
-            <Row style={{ margin: 10 }}>
-              <Text style={{ flex: 1.5 }}>Kadar:</Text>
-              <View style={{ flex: 0.2 }} />
-              <Item
+          <Row style={{ margin: 10 }}>
+            <Text style={{ flex: 1.5 }}>Berat: </Text>
+            <View style={{ flex: 0.2 }} />
+            <Item
+              style={{
+                flex: 1.5,
+                alignSelf: 'center',
+                height: '3vh',
+                backgroundColor: '#FFF',
+                width: '20vw',
+              }}
+              regular
+            >
+              <Input
+                style={{ height: '3vh' }}
+                value={state.berat}
+                onChangeText={(text) => setter('berat', text, true)}
+              />
+            </Item>
+            <Text style={{ marginHorizontal: 10 }}>gr</Text>
+            <View style={{ flex: 2, flexGrow: 10, flexBasis: 25 }} />
+          </Row>
+
+          <Row style={{ margin: 10 }}>
+            <Text style={{ flex: 1.5 }}>Kadar:</Text>
+            <View style={{ flex: 0.2 }} />
+            <Item
+              style={{
+                flex: 1.5,
+                alignSelf: 'center',
+                height: '3vh',
+                backgroundColor: '#FFF',
+                width: '20vw',
+              }}
+              regular
+            >
+              <Input
+                style={{ height: '3vh' }}
+                value={state.kadar}
+                onChangeText={(text) => setter('kadar', text, true)}
+              />
+            </Item>
+            <Text style={{ marginHorizontal: 10 }}>%</Text>
+            <View style={{ flex: 2, flexGrow: 10, flexBasis: 25 }} />
+          </Row>
+
+          <Row style={{ margin: 10 }}>
+            <Text style={{ flex: 1.5 }}>Posisi:</Text>
+            <View style={{ flex: 0.2 }} />
+            <Form
+              style={{
+                flex: 2,
+                alignSelf: 'center',
+                height: '3vh',
+                backgroundColor: '#FFF',
+                width: '20vw',
+              }}
+            >
+              <Picker
+                mode="dropdown"
+                // placeholder="Select your SIM"
+                // placeholderStyle={{ color: '#bfc6ea' }}
+                // placeholderIconColor="#007aff"
+                style={{ width: undefined }}
+                selectedValue={state.posisi}
+                onValueChange={(value) => setter('posisi', value)}
+              >
+                <Picker.Item label="Buat Baru" value="-" />
+                {posisiList.map((value, index) => (
+                  <Picker.Item key={index} label={value} value={value} />
+                ))}
+              </Picker>
+            </Form>
+            <div
+              style={{
+                display: state.posisi === '-' ? 'block' : 'none',
+                marginLeft: '5px',
+              }}
+            >
+              <Input
                 style={{
-                  flex: 1.5,
-                  alignSelf: "center",
-                  height: "3vh",
-                  backgroundColor: "#FFF",
-                  width: "20vw"
+                  borderWidth: '1px',
                 }}
-                regular
-              >
-                <Input style={{ height: "3vh" }} />
-              </Item>
-              <Text style={{ marginHorizontal: 10 }}>%</Text>
-              <View style={{ flex: 2, flexGrow: 10, flexBasis: 25 }} />
-            </Row>
+                value={state.posisiBaru}
+                onChangeText={(text) => setter('posisiBaru', text)}
+              />
+            </div>
+            <View style={{ flex: 2, flexGrow: 10, flexBasis: 25 }} />
+          </Row>
 
-            <Row style={{ margin: 10 }}>
-              <Text style={{ flex: 1.5 }}>Posisi:</Text>
-              <View style={{ flex: 0.2 }} />
-              <Form
-                style={{
-                  flex: 2,
-                  alignSelf: "center",
-                  height: "3vh",
-                  backgroundColor: "#FFF",
-                  width: "20vw"
-                }}
-              >
-                <Picker
-                  mode="dropdown"
-                  placeholder="Select your SIM"
-                  placeholderStyle={{ color: "#bfc6ea" }}
-                  placeholderIconColor="#007aff"
-                  style={{ width: undefined }}
-                  selectedValue={this.state.selected2}
-                  onValueChange2={this.onValueChange2.bind(this)}
-                >
-                  <Picker.Item label="Pilih" value="key0" />
-                  <Picker.Item label="A1" value="key1" />
-                  <Picker.Item label="A2" value="key2" />
-                  <Picker.Item label="B1" value="key3" />
-                  <Picker.Item label="B2" value="key4" />
-                </Picker>
-              </Form>
-              <View style={{ flex: 2, flexGrow: 10, flexBasis: 25 }} />
-            </Row>
+          {/*<Row style={{ margin: 10 }}>
+            <Text style={{ flex: 1.5 }}>Gambar:</Text>
+            <View style={{ flex: 0.2 }} />
+            <Button
+              rounded
+              light
+              style={{
+                flex: 1,
+                alignSelf: 'center',
+                height: '3vh',
+                justifyContent: 'center',
+                backgroundColor: '#D9D9D9',
+                width: '20vw',
+              }}
+            >
+              <Text>Unggah</Text>
+            </Button>
+            <View style={{ flex: 2, flexGrow: 10, flexBasis: 25 }} />
+          </Row>*/}
 
-            <Row style={{ margin: 10 }}>
-              <Text style={{ flex: 1.5 }}>Gambar:</Text>
-              <View style={{ flex: 0.2 }} />
-              <Button
-                rounded
-                light
-                style={{
-                  flex: 1,
-                  alignSelf: "center",
-                  height: "3vh",
-                  justifyContent: "center",
-                  backgroundColor: "#D9D9D9",
-                  width: "20vw"
-                }}
-              >
-                <Text>Unggah</Text>
-              </Button>
-              <View style={{ flex: 2, flexGrow: 10, flexBasis: 25 }} />
-            </Row>
+          <Row style={{ margin: 10 }}>
+            <Text style={{ flex: 1.5 }}>Harga Beli:</Text>
+            <View style={{ flex: 0.2 }} />
 
-            <Row style={{ margin: 10 }}>
-              <Text style={{ flex: 1.5 }}>Harga Beli:</Text>
-              <View style={{ flex: 0.2 }} />
+            <Item
+              style={{
+                flex: 2,
+                alignSelf: 'center',
+                height: '3vh',
+                backgroundColor: '#FFF',
+                width: '20vw',
+              }}
+              regular
+            >
+              <Input style={{ height: '3vh' }} />
+            </Item>
+            <View style={{ flex: 2, flexGrow: 10, flexBasis: 25 }} />
+          </Row>
 
-              <Item
-                style={{
-                  flex: 2,
-                  alignSelf: "center",
-                  height: "3vh",
-                  backgroundColor: "#FFF",
-                  width: "20vw"
-                }}
-                regular
-              >
-                <Input style={{ height: "3vh" }} />
-              </Item>
-              <View style={{ flex: 2, flexGrow: 10, flexBasis: 25 }} />
-            </Row>
+          <Row style={{ margin: 10 }}>
+            <View style={{ flex: 3 }} />
+            <Button rounded light style={{ backgroundColor: '#D9D9D9' }}>
+              <Text>Simpan</Text>
+            </Button>
+            <View style={{ flex: 1, flexGrow: 10, flexBasis: 25 }} />
+          </Row>
+        </Grid>
+      </Content>
+    </Container>
+  );
+};
 
-            <Row style={{ margin: 10 }}>
-              <View style={{ flex: 3 }} />
-              <Button rounded light style={{ backgroundColor: "#D9D9D9" }}>
-                <Text>Simpan</Text>
-              </Button>
-              <View style={{ flex: 1, flexGrow: 10, flexBasis: 25 }} />
-            </Row>
-          </Grid>
-        </Content>
-      </Container>
-    );
-  }
-}
+export default TambahItem;
