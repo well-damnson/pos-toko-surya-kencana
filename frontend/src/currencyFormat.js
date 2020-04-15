19aec94a9cbb67881cb50011bd7e2db2613ac5c2
AttreaVe <johansiautama@gmail.com>
2020-04-15 11:36:11 +0700

    currency format update

--- frontend/src/currencyFormat.js
+++ frontend/src/currencyFormat.js
@@ -1,5 +1,40 @@
 export const currency = (value) =>
-  new Intl.NumberFormat("id-ID", {
-    style: "currency",
-    currency: "IDR",
+  new Intl.NumberFormat('id-ID', {
+    style: 'currency',
+    currency: 'IDR',
   }).format(value);
+
+export const curr = (
+  amount,
+  decimalCount = 2,
+  decimal = '.',
+  thousands = ',',
+  sign = 'Rp. ',
+) => {
+  try {
+    decimalCount = Math.abs(decimalCount);
+    decimalCount = isNaN(decimalCount) ? 2 : decimalCount;
+
+    const negativeSign = amount < 0 ? '-' : '';
+
+    let i = parseInt(
+      (amount = Math.abs(Number(amount) || 0).toFixed(decimalCount)),
+    ).toString();
+    let j = i.length > 3 ? i.length % 3 : 0;
+
+    return (
+      sign +
+      negativeSign +
+      (j ? i.substr(0, j) + thousands : '') +
+      i.substr(j).replace(/(\d{3})(?=\d)/g, '$1' + thousands) +
+      (decimalCount
+        ? decimal +
+          Math.abs(amount - i)
+            .toFixed(decimalCount)
+            .slice(2)
+        : '')
+    );
+  } catch (e) {
+    console.log(e);
+  }
+};
